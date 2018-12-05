b96de4da5ada5f122edfb2fdc0da6874c08e4352
Luis Bastidas <luisbastir@gmail.com>
2018-12-05 12:53:39 -0600

    add events logger in view

--- app.js
+++ app.js
@@ -3,23 +3,35 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   methods: {
     startGame: function() {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function() {
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player hits Monster for " + damage
+      })
       if (this.checkWin()) {
         return;
       }
       this.monsterAttacks()
     },
     specialAttack: function() {
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      var damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player hits Monster hard for " + damage
+      })
       if (this.checkWin()) {
         return;
       }
@@ -31,14 +43,23 @@ new Vue({
       } else {
         this.playerHealth = 100;
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player heals for 10"
+      })
       this.monsterAttacks();
     },
     giveUp: function() {
       this.gameIsRunning = false;
     },
     monsterAttacks: function() {
-      this.playerHealth -= this.calculateDamage(5, 12);
-      this.checkWin()
+      var damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
+      this.turns.unshift({
+        isPlayer: false,
+        text: "Monster hits Player for " + damage
+      })
+      this.checkWin();
     },
     calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
@@ -52,7 +73,7 @@ new Vue({
         }
         return true;
       } else if (this.playerHealth <= 0) {
-        if (confirm("You won! New Game?")) {
+        if (confirm("You loose! New Game?")) {
           this.startGame()
         } else {
           this.gameIsRunning = false;
